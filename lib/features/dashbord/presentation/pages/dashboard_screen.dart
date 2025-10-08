5151811180de15ff86478533f6774796d77fca37
Zaminur <hasanzminur@gmail.com>
2025-10-08 15:14:27 +0600

    dashboard add

---
+++ lib/features/dashbord/presentation/pages/dashboard_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_arch/features/dashbord/presentation/bloc/post_bloc.dart';
+import 'package:flutter_clean_arch/features/dashbord/presentation/bloc/post_state.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<PostBloc, PostState>(
+        builder: (context,state){
+          if(state is PostInitialState){
+            return Center(
+              child: ElevatedButton(
+                onPressed: (){}, 
+                child: Text('Load Posts')
+                ),
+            );
+          }
+          else if(state is PostLoadingState){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else if(state is PostSuccessState){
+            return ListView.builder(
+              itemCount: state.post.length,
+              itemBuilder: (context,index){
+                final post = state.post[index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.body),
+                );
+              }
+              );
+          }
+          else if (state is PostErrorState){
+            return Center(
+              child: Text(state.message),
+            );
+          }
+          else{
+            return SizedBox.shrink();
+          }
+        },
+      ),
+    );
+  }
+}
